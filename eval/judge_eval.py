7dd393d9d8c0310c21aefa9c585560a4c3aec7e2
Mikhail-Repkin <mikhail.repkin90@gmail.com>
2025-01-22 23:05:35 +0300

    VQA judge class added

--- eval/judge_eval.py
+++ eval/judge_eval.py
@@ -161,3 +161,147 @@ class ModelComparisonJudge:
                 raise Exception(f"Request failed: {e}")
 
         raise Exception("Failed to parse LLM response after multiple retries.")
+
+
+class VQAModelJudge:
+    """Класс для судейской оценки ответов на VQA задачу."""
+
+    def __init__(
+        self,
+        model,
+        gpt4_api_key,
+        system_msg,
+        url="https://gptunnel.ru/v1/chat/completions",
+    ):
+        self.model = model
+        self.url = url
+
+        if not gpt4_api_key:
+            raise ValueError("API key not found.")
+
+        self.api_key = gpt4_api_key
+        self.system_prompt = system_msg
+
+    def parse_llm_response(self, response_text):
+        """Парсит JSON-ответ из текста LLM."""
+        try:
+            # Извлекаем JSON часть из текста ответа
+            json_start_index = response_text.find("{")
+            json_end_index = response_text.rfind("}") + 1
+
+            if json_start_index == -1 or json_end_index == -1:
+                raise ValueError("JSON data not found in the response text.")
+
+            json_data = response_text[json_start_index:json_end_index]
+            parsed_response = json.loads(json_data)
+            return parsed_response
+        except Exception as e:
+            raise ValueError(f"Failed to parse LLM response: {e}")
+
+    def judge_responses(
+        self,
+        image_url,
+        user_msg,
+        answer_reference,
+        answer_candidate,
+        max_retries=3
+    ):
+        """Метод для судейской оценки ответа на вопрос об изображении.
+        Params:
+        - image_url: путь к изображению
+        - user_msg: вопрос пользователя
+        - answer_reference: эталонный человеческий ответ
+        - answer_candidate: ответ тестируемой модели
+        - max_retries: макс. количество повторов
+
+        Returns:
+        {
+            "Candidate_score": int,
+            "Candidate_hallucination": int,
+            "Explanation": str
+        }
+
+        """
+
+        answers = [answer_reference, answer_candidate]
+
+        headers = {
+            "Authorization": f"Bearer {self.api_key}",
+            "Content-Type": "application/json",
+        }
+
+        user_prompt = (
+            "[User question]\n"
+            f"{user_msg}\n\n"
+            "[The start of Human reference answer]\n"
+            f"{answers[0]}\n"
+            "[The end of Human reference answer]\n\n"
+            "[The start of Candidate model answer]\n"
+            f"{answers[1]}\n"
+            "[The end of Candidate model answer]"
+        )
+
+        # Формируем запрос к GPTunnel (аналог Chat Completion)
+        # "detail": "low" will enable the "low res" mode.
+        # The model will receive a low-res 512px x 512px version of the image,
+        # and represent the image with a budget of 85 tokens.
+        # This allows the API to return faster responses and consume
+        # fewer input tokens for use cases that do not require high detail.
+        data = {
+            "model": self.model,
+            "messages": [
+                {"role": "system", "content": self.system_prompt},
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "image_url",
+                            "image_url": {"url": image_url, "detail": "low"},
+                        },
+                        {"type": "text", "text": user_prompt},
+                    ],
+                },
+            ],
+            "useWalletBalance": True,
+        }
+
+        retry_count = 0
+
+        while retry_count < max_retries:
+            try:
+                response = requests.post(
+                    self.url,
+                    headers=headers,
+                    data=json.dumps(data),
+                    timeout=None,
+                )
+                if response.status_code == 200:
+                    resp = response.json()
+                    print("Total Cost:", resp["usage"]["total_cost"])
+                    print("Total Tokens:", resp["usage"]["total_tokens"])
+
+                    try:
+                        result = resp["choices"][0]["message"]["content"]
+                        parsed_result = self.parse_llm_response(result)
+                        print("result:", result)
+                        print("result:", parsed_result)
+
+                        final_result = {
+                            "Candidate_score": parsed_result["Candidate_score"],
+                            "Candidate_hallucination": parsed_result[
+                                "Candidate_hallucination"
+                            ],
+                            "Explanation": parsed_result["Explanation"],
+                        }
+
+                        return final_result
+                    except ValueError as e:
+                        print(f"Parsing error on attempt {retry_count + 1}: " f"{e}")
+                        retry_count += 1
+            except requests.exceptions.Timeout:
+                print("Timeout error occurred, retrying...")
+                retry_count += 1
+            except Exception as e:
+                raise Exception(f"Request failed: {e}")
+
+        raise Exception("Failed to parse LLM response after multiple retries.")
